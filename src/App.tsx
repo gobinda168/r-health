ef8e0d547b16b5f6b3932dda764f2865fd9cd51e
Gobinda Das <gobinda168@gmail.com>
2021-01-22 18:19:39 +0530

    Implement: fullpage with blue accent

--- src/App.tsx
+++ src/App.tsx
@@ -7,32 +7,11 @@ import Offer from './pages/offer/Offer';
 import PackageDetails from './pages/packageDetails/PackageDetails';
 
 const App = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/">
-        <PackageDetails />
-      </Route>
-      <Route path="/offer">
-        <Offer />
-      </Route>
-      <Route path="/services">
-        <Services />
-      </Route>
-    </Switch>
-    <div className={styles.appContainer}>
-      <div className={styles.navigation}>
-        <Link to="/">
-          <Button>Package Details</Button>
-        </Link>
-        <Link to="/offer">
-          <Button>Offer</Button>
-        </Link>
-        <Link to="/services">
-          <Button>Services </Button>
-        </Link>
-      </div>
-    </div>
-  </Router>
+  <>
+    <PackageDetails />
+    <Services />
+    <Offer />
+  </>
 );
 
 export default App;
--- src/pages/services/Services.tsx
+++ src/pages/services/Services.tsx
@@ -3,22 +3,26 @@ import { AiFillDollarCircle, AiFillHeart } from 'react-icons/ai';
 import Card from '../../components/card/Card';
 import styles from './Services.module.scss';
 
-const Services = () => (
-  <div className={styles.services}>
-    <div className={styles.title}>OR You can buy individual services</div>
-    <div className={styles.container}>
-      <Card
-        icon={<AiFillDollarCircle size="3rem" color="#512e3c" />}
-        title="Full Service Payroll"
-        subTitle="Pay your people in minute with reliable, modern payroll"
-      />
-      <Card
-        icon={<AiFillHeart size="3rem" color="#e4836b" />}
-        title="Benefits Administration"
-        subTitle="Easily manage your medical, dental, vision, 401K, and more in one place"
-      />
+const Services = () => {
+  const BLUE_COLOR = '#2b8fff';
+  const PINK_COLOR = '#e4836b';
+  return (
+    <div className={styles.services}>
+      <div className={styles.title}>OR You can buy individual services</div>
+      <div className={styles.container}>
+        <Card
+          icon={<AiFillDollarCircle size="3rem" color={BLUE_COLOR} />}
+          title="Full Service Payroll"
+          subTitle="Pay your people in minute with reliable, modern payroll"
+        />
+        <Card
+          icon={<AiFillHeart size="3rem" color={PINK_COLOR} />}
+          title="Benefits Administration"
+          subTitle="Easily manage your medical, dental, vision, 401K, and more in one place"
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Services;
