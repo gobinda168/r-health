34bb61cbc979eeaa2a2185dee21a0324ab8e9368
Gobinda Das <gobinda168@gmail.com>
2021-01-22 16:46:14 +0530

    Before merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const App = () => (
   <div className="App">
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
       <p>
         Edit
         <code>src/App.js</code>
